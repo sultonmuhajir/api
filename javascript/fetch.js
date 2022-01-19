16a27eaeed72568a7bd73e341fd9cd13c8b16ace
Sulton Muhajir <sulton.muhajir28@gmail.com>
2022-01-19 12:31:39 +0700

    first commit

---
+++ javascript/fetch.js
@@ -0,0 +1,19 @@
+fetch('https://feriirawan-api.herokuapp.com/list/symbols/province/150')
+   .then(res => res.json())
+   .then(data => {
+      const lambang = data.lambang;
+      let dataLambang = '';
+      lambang.forEach(el => {
+         dataLambang += `
+         <div class="col-md-3 m-3">
+            <div class="card">
+               <h5 class="card-header text-center">${el.title}</h5>
+               <div class="card-body text-center">
+                  <img src="${el.url}" alt="${el.title}">
+               </div>
+            </div>
+         </div>
+         `
+      });
+      document.querySelector(".row").innerHTML = dataLambang;
+   })
